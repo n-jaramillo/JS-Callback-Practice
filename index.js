82ceefc0a9e428f7bb9e058aac3be951f49181db
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-23 22:00:34 -0700

    step three, added event listeners for changing direction with arrow key presses

--- index.js
+++ index.js
@@ -20,8 +20,29 @@ setInterval(function(){
         y = y - 1
     }
     character.style.left = x + 'px'
-    character.style.left = y + 'px'
+    character.style.bottom = y + 'px'
 }, 1)
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft') {
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp') {
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight') {
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown') {
+        direction = 'south'
+    }
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
 
 move(character).to(100, 250)
 
