b23b2e1c4ca178b87028f7dda3ca05daff7f4b86
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-24 21:37:41 -0700

    added single quotes to null in keyup function
    fixes character constantly moving even when keyup event occurs

--- move.js
+++ move.js
@@ -52,7 +52,7 @@ function move(element) {
         })
         
         document.addEventListener('keyup', function(e){
-            direction = null
+            direction = 'null'
             handleDirectionChange(direction)
         })
     }
